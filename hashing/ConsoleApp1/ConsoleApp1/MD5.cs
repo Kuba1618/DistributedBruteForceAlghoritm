d768b39d69b1b81fe3854c39ab9507ca4b888f77
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-04-18 11:59:09 +0200

    Add Md5 and SHA256 hashing alghoritms

---
+++ hashing/ConsoleApp1/ConsoleApp1/MD5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MD5
+    {
+        public static void Main()
+        {
+            string password = "test";
+            string s = CreateMD5(password);
+
+            Console.WriteLine($"MD5 hash for {password}: {s}");
+            Console.ReadLine();
+        }
+
+        public static string CreateMD5(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                //return Convert.ToHexString(hashBytes); // .NET 5 +
+
+                // Convert the byte array to hexadecimal string 
+                 StringBuilder sb = new System.Text.StringBuilder();
+                 for (int i = 0; i < hashBytes.Length; i++)
+                 {
+                     sb.Append(hashBytes[i].ToString("X2"));
+                 }
+                 return sb.ToString();
+            }
+        }
+    }
+}
+
+
---
+++ hashing/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Program
+{
+    public static void Main1()
+    {
+        string password = "test";
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            string hash = GetHash(sha256Hash, password);
+
+            Console.WriteLine($"The SHA256 hash of '{password}' : {hash}.");
+
+            Console.WriteLine("Verifying the hash...");
+
+            if (VerifyHash(sha256Hash, password, hash))
+            {
+                Console.WriteLine("The hashes are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The hashes are not same.");
+            }
+        }
+        Console.ReadLine();
+    }
+
+    private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+    {
+        byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+        var sBuilder = new StringBuilder();
+        
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+
+        return sBuilder.ToString();
+    }
+
+    private static bool VerifyHash(HashAlgorithm hashAlgorithm, string input, string hash)
+    {
+        var hashOfInput = GetHash(hashAlgorithm, input);
+
+        StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+        return comparer.Compare(hashOfInput, hash) == 0;
+    }
+}
